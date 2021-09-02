850f32e72319b7507eeb2ddfb9b7f3ae7d1a855b
jhwilliampape <jhwilliampape@gmail.com>
2021-09-02 11:50:19 -0500

    Adding makeStyles to TurnTable

--- src/components/TurnTable/TurnTable.tsx
+++ src/components/TurnTable/TurnTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import { ListItem, List, withStyles } from '@material-ui/core';
+import { ListItem, List, makeStyles } from '@material-ui/core';
 
 import { LayeredImage } from 'react-layered-image';
 
@@ -10,55 +10,60 @@ import table_arm from '../../assets/images/record_needle_arm.png';
 
 
 
-const style = {
-    position: "absolute" as "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-};
+// const style = {
+//     position: "absolute" as "absolute",
+//     top: 0,
+//     right: 0,
+//     bottom: 0,
+//     left: 0,
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+// };
 
-const layers = [
-    `${turntable}`,
-    `${record}`,
-    `${table_arm}`
-];
+// const layers = [
+//     `${turntable}`,
+//     `${record}`,
+//     `${table_arm}`
+// ];
 
-render (
-    <div style={style}>
-        <LayeredImage layers={layers} style={{ width: 400 }} />
-    </div>
-    ,document.getElementById("root"),
-);
-
-
+// render (
+//     <div style={style}>
+//         <LayeredImage layers={layers} style={{ width: 400 }} />
+//     </div>
+//     ,document.getElementById("root"),
+// );
 
 
-// const StyledListItem = withStyles({
 
-//     root: {
 
-//         backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${turntable});`,
-//         width: '100%',
-//         height: '100%',
-//         backgroundSize: 'cover',
-//         backgroundRepeat: 'no-repeat',
-//         backgroundPosition: 'center',
-//         position: 'absolute',
-//     }
-    
-// })(ListItem);
+// const StyledListItem = styled(ListItem)({
 
-// function TurnTable() {
+// });
 
-//     return (
-//         <List component = "nav">
-//             <StyledListItem>default</StyledListItem>
+const useStyles = makeStyles({
+    rootListItem: {
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${turntable});`,
+        width: '100%',
+        height: '100%',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        position: 'absolute',
+    }
+})
+
+function TurnTable() {
+
+    const classes = useStyles()
+
+    return (
+        <List component = "nav">
+            <ListItem className={classes.rootListItem} >
+                default
+            </ListItem>
             
-//         </List>
-//     );
-// }
-// export default TurnTable;
+        </List>
+    );
+}
+export default TurnTable;
